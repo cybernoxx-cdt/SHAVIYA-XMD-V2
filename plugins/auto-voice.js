f280900d089a708468db928867caba5dbb3d2f85
cybernoxx-cdt <cybernoxx7489@gmail.com>
2026-03-22 22:02:01 +0530

    Create auto-voice.js

---
+++ plugins/auto-voice.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+const config = require('../config')
+const {cmd , commands} = require('../command')
+
+//auto_voice
+cmd({
+  on: "body"
+},    
+async (robin, mek, m, { from, body, isOwner }) => {
+    const filePath = path.join(__dirname, '../ranumitha_data/autovoice.json');
+    const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+    for (const text in data) {
+        if (body.toLowerCase() === text.toLowerCase()) {
+            
+            if (config.AUTO_VOICE === 'true') {
+                if (isOwner) return;        
+                await robin.sendPresenceUpdate('recording', from);
+                await robin.sendMessage(from, { audio: { url: data[text] }, mimetype: 'audio/mpeg', ptt: true }, { quoted: mek });
+            }
+        }
+    }                
+});
